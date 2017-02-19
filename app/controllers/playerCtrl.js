3dfb57cb5f04785c5c69d0e31141617b047215a1
Brian Merchant <btmerchant@comcast.net>
2017-02-19 13:23:40 -0600

    Delete buttons working! and some list formating

--- app/controllers/playerCtrl.js
+++ app/controllers/playerCtrl.js
@@ -20,16 +20,44 @@ MusApp.controller('PlayerCtrl', ['$scope', '$location', 'Auth', '$firebaseArray'
 
     this.addSong = function() {
       console.log("addSong Function");
+      if (!this.newSong.artist) {
+        this.newSong.artist = null;
+      }
+      if (!this.newSong.composer) {
+        this.newSong.composer = null;
+      }
+      if (!this.newSong.key) {
+        this.newSong.key = null;
+      }
+      if (!this.newSong.leadVoc) {
+        this.newSong.leadVoc = null;
+      }
+      if (!this.newSong.tempo) {
+        this.newSong.tempo = null;
+      }
+      if (!this.newSong.status) {
+        this.newSong.status = null;
+      }
       songs.$add({
         uid: this.user,
-        title: this.newSong.title
+        title: this.newSong.title,
+        artist: this.newSong.artist,
+        composer: this.newSong.composer,
+        key: this.newSong.key,
+        leadVoc: this.newSong.leadVoc,
+        tempo: this.newSong.tempo,
+        status: this.newSong.status
       });
       console.log('Added Song');
     };
 
-    this.deleteSong = function() {
+     this.deleteSong = function(id) {
+      console.log(id);
+      var songRef = new Firebase('https://musicon.firebaseio.com/songs' + '/' + id);
+      console.log("songRef", songRef);
       console.log("deleteSong function");
-      songs.$remove();
+ 
+      songRef.remove();
       console.log("delete function run");
     };
 
